5b4bc1acecee2b66064ded3b6b2d0591e97c176f
NaheedRayan <naheed28ray@protonmail.com>
2021-05-17 00:27:26 +0600

    added redirect in place of render

--- routes/signup.js
+++ routes/signup.js
@@ -1,6 +1,7 @@
 const express = require("express");
 let router = express.Router();
-
+// for database
+const DbService = require("../models/database");
 
 
 
@@ -8,19 +9,37 @@ router.route('/')
     .get((req, res) => {
         console.log("inside signup.js GET");
         res.render("pages/signup");
+
+        const db = DbService.getServiceInstance();
+
+
+        const result = db.getAllData();
+        result
+            .then(data => {
+                console.log(data)
+                
+
+            })
+            .catch(err => console.log(err))
+
+        
+
     })
     .post((req, res) => {
         console.log("inside signup.js POST");
         console.log(req.body);
 
+
+
         if (req.body.username == "Naheed")
             req.flash("success", "User successfully registered");
         else
             req.flash("error", "Something went wrong!!");
+     
+        // res.locals.message = req.flash();
 
-        res.locals.message = req.flash();
-
-        res.render("pages/signup");
+        res.redirect("/home");
+    
     })
 
 
