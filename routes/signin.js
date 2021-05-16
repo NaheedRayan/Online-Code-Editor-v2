c1f6d60eaae282e1aa0f0a6af92371a8874c4275
NaheedRayan <naheed28ray@protonmail.com>
2021-05-17 00:28:17 +0600

    fixed bugs

--- routes/signin.js
+++ routes/signin.js
@@ -1,11 +1,16 @@
 const express = require("express");
 let router = express.Router();
 
+const DbService = require("../models/database");
+
+
 
 router.route('/')
     .get((req, res) => {
         console.log("inside signin.js GET");
         res.render("pages/signin");
+
+        
     })
     .post((req, res) => {
         console.log("inside signin.js POST");
