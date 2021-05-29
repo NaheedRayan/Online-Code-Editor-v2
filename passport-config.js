5b0488862046101e7af8eaf5d987b0e2c611e936
NaheedRayan <naheed28ray@protonmail.com>
2021-05-29 22:59:05 +0600

    indentation

--- passport-config.js
+++ passport-config.js
@@ -1,11 +1,12 @@
-const LocalStrategy = require('passport-local').Strategy; // load all the things we need
+// load all the things we need
+const LocalStrategy = require('passport-local').Strategy;
 const DbService = require("./models/database");
 
 
 
 
 
-// expose this function to our app using module.exports
+// expose this function to our app.js using module.exports
 module.exports = function (passport) {
 
     // =========================================================================
@@ -29,13 +30,13 @@ module.exports = function (passport) {
             console.log(result)
             if (result.length) {
                 // all is well, return successful user
-                return done(null , result[0])
+                return done(null, result[0])
             } else {
-                return done(null, false); 
+                return done(null, false);
             }
         }).catch(err => {
             console.log(err)
-            return done(null, false, req.flash("error", "Something went wrong")); // create the loginMessage and save it to session as flashdata
+            return done(null, false, req.flash("error", "Something went wrong")); // create the Message and save it to session as flashdata
         })
     });
 
@@ -49,87 +50,66 @@ module.exports = function (passport) {
         'local-signup',
         new LocalStrategy({
             // by default, local strategy uses username and password, we will override with email
-            usernameField : 'email' ,
-            passwordField : 'password' , 
-            passReqToCallback : true // allows us to pass back the entire request to the callback
-        },function(req , username , email , done){
-            const db = DbService.getServiceInstance();
+            usernameField: 'email',
+            passwordField: 'password',
+            passReqToCallback: true // allows us to pass back the entire request to the callback
+        }, function (req, username, email, done) {
+            // if the remember button is clicked
+            if (req.body.remember) {
+                req.session.cookie.maxAge = 7 * 24 * 60 * 60 * 1000; // remember for 7 days
+            } else {
+                req.session.cookie.expires = false;
+            }
+            const db = DbService.getServiceInstance();//getting db instance
 
-        if (req.body.username && req.body.email) {
+            if (req.body.username && req.body.email) {
 
-            let username = req.body.username;
-            let email = req.body.email;
-            let password = req.body.password;
+                let username = req.body.username;
+                let email = req.body.email;
+                let password = req.body.password;
 
-            const result1 = db.getUsername(username);
-            const result2 = db.getEmail(email);
+                const result1 = db.getUsername(username);
+                const result2 = db.getEmail(email);
 
-            // getting all the promises and then processing it
-            Promise.all([result1, result2]).then(data => {
+                // getting all the promises and then processing it
+                Promise.all([result1, result2]).then(data => {
 
-                    if (data[0].length && data[1].length == 0) {
-                        // console.log(data[0]);
-                        // req.flash("error", "The username exists!!");
-                        // res.redirect("/signup");
-                        return done(null, false, req.flash("error", "The username exists!!")); // create the loginMessage and save it to session as flashdata
+                        if (data[0].length && data[1].length == 0) {
+                            return done(null, false, req.flash("error", "The username exists!!")); // create the Message and save it to session as flashdata
+                        } else if (data[0].length == 0 && data[1].length) {
+                            return done(null, false, req.flash("error", "The email exists!!")); // create the Message and save it to session as flashdata
+                        } else if (data[0].length && data[1].length) {
+                            return done(null, false, req.flash("error", "The username and email already exists!!")); // create the Message and save it to session as flashdata
+                        } else if (req.body.password != req.body.confirm_password) {
+                            return done(null, false, req.flash("error", "Passwords do not match")); // create the Message and save it to session as flashdata
+                        } else {
+                            let val = [
+                                [username, email, password]
+                            ];
+                            let result = db.saveNewUserData(val);
+                            result
+                                .then(data => {
+                                    // making a newUser object for passing it
+                                    let newUser = {
+                                        username: req.body.username,
+                                        email: req.body.email,
+                                        password: req.body.password,
+                                    }
+                                    newUser.id = data.insertId; // we have to add the inserted id to the newUser
+                                    return done(null, newUser, req.flash("success", "Signup Successful")) //we have to pass the object for serialization
+                                })
+                                .catch(err => {
+                                    console.log(err);
+                                    return done(null, false, req.flash("error", "Something went wrong")); // create the Message and save it to session as flashdata
+                                })
+                        }
+                    })
+                    .catch(err => console.log(err))
 
-                    } else if (data[0].length == 0 && data[1].length) {
-                        // console.log(data[1]);
-                        // req.flash("error", "The email exists!!");
-                        // res.redirect("/signup");
-                        return done(null, false, req.flash("error", "The email exists!!")); // create the loginMessage and save it to session as flashdata
-
-                    } else if (data[0].length && data[1].length) {
-                        // console.log(data[1]);
-                        // req.flash("error", "The username and email already exists!!");
-                        // res.redirect("/signup");
-                        return done(null, false, req.flash("error", "The username and email already exists!!")); // create the loginMessage and save it to session as flashdata
-
-                    } else if (req.body.password != req.body.confirm_password) {
-                        // req.flash("error", "Passwords do not match");
-                        // res.redirect("/signup");
-                        return done(null, false, req.flash("error", "Passwords do not match")); // create the loginMessage and save it to session as flashdata
-
-                    } else {
-                        let val = [
-                            [username, email, password]
-                        ];
-                        let result = db.saveNewUserData(val);
-                        result
-                            .then(data => {
-                                // console.log(data)
-                                // req.flash("success", "User successfully registered");
-                                // res.redirect("/editor")
-                                // all is well, return successful user
-                                let newUser = {
-                                    username:req.body.username , 
-                                    email: req.body.email,
-                                    password: req.body.password,
-
-                                }
-                                newUser.id = data.insertId;// we have to add the inserted id to the newUser
-                                console.log(newUser)
-                                return done(null , newUser ,req.flash("success", "Signup Successful"))//we have to pass the object for serialization
-                    
-                            })
-                            .catch(err => {
-                                console.log(err);
-                                // res.redirect("/signup");
-                                return done(null, false, req.flash("error", "Something went wrong")); // create the loginMessage and save it to session as flashdata
-
-                            })
-
-                    }
-
-                })
-                .catch(err => console.log(err))
-
-        } else {
-            req.flash("error", "Something went wrong!!");
-            res.redirect("/signup");
-        }
-        
-            
+            } else {
+                req.flash("error", "Something went wrong!!");
+                res.redirect("/signup");
+            }
         })
     )
     // =========================================================================
@@ -137,7 +117,6 @@ module.exports = function (passport) {
     // =========================================================================
     // we are using named strategies since we have one for login and one for signup
     // by default, if there was no name, it would just be called 'local'
-
     passport.use(
         'local-login',
         new LocalStrategy({
@@ -147,12 +126,10 @@ module.exports = function (passport) {
                 passReqToCallback: true // allows us to pass back the entire request to the callback
             },
             function (req, username_or_email, password, done) { // callback with email and password from our form
-
-                console.log("++++++++++++++++++++++++++++++++++++++")
-                console.log(req.body.remember)//on
+                // console.log(req.body.remember) //on
                 // if the remember button is clicked
                 if (req.body.remember) {
-                    req.session.cookie.maxAge = 7*24*60*60*1000; // remember for 7 days
+                    req.session.cookie.maxAge = 7 * 24 * 60 * 60 * 1000; // remember for 7 days
                 } else {
                     req.session.cookie.expires = false;
                 }
@@ -164,16 +141,14 @@ module.exports = function (passport) {
                     console.log(result)
                     if (result.length) {
                         // all is well, return successful user
-                        return done(null , result[0],req.flash("success", "Login Successful"))
+                        return done(null, result[0], req.flash("success", "Login Successful"))
                     } else {
-                        return done(null, false, req.flash("error", "Invalid Username or Password")); // create the loginMessage and save it to session as flashdata
+                        return done(null, false, req.flash("error", "Invalid Username or Password")); // create the Message and save it to session as flashdata
                     }
                 }).catch(err => {
                     console.log(err)
-                    return done(null, false, req.flash("error", "Something went wrong")); // create the loginMessage and save it to session as flashdata
+                    return done(null, false, req.flash("error", "Something went wrong")); // create the Message and save it to session as flashdata
                 })
-
-               
             })
     );
 };
--- routes/signin.js
+++ routes/signin.js
@@ -6,15 +6,12 @@ const passport = require("passport");
 
 require('../passport-config')(passport); // pass passport for configuration
 
-
-
 router.get("/", (req, res) => {
     console.log("inside signin.js GET");
     res.render("pages/signin");
 
 
 })
-
 
 // process the login form
 router.post('/', passport.authenticate('local-login', {
@@ -23,60 +20,8 @@ router.post('/', passport.authenticate('local-login', {
         failureFlash: true // allow flash messages
     }),
     function (req, res) {
-        
-        // if (req.body.remember) {
-        //     req.session.cookie.maxAge = 1000 * 60 * 9;
-        // } else {
-        //     req.session.cookie.expires = false;
-        // }
         res.redirect('/editor');
     });
 
-// router.post("/", (req, res) => {
-//     console.log("inside signin.js POST");
-//     // console.log(req.body);
-//     // res.render("pages/signin");
-//     const db = DbService.getServiceInstance();
-
-//     if (req.body.username_or_email && req.body.password) {
-
-//         let username_or_email = req.body.username_or_email;
-//         let password = req.body.password;
-
-//         const result = db.getPassword_for_email_or_username(username_or_email, password);
-
-
-//         result.then(result => {
-//             console.log(result)
-//             if (result.length) {
-//                 req.flash("success", "Login Successful");
-//                 res.redirect("/editor")
-//             } else {
-//                 req.flash("error", "Invalid Username or Password");
-//                 res.redirect("/signin");
-//             }
-//         }).catch(err => {
-//             console.log(err)
-//             res.redirect("/signin")
-//         })
-
-//     } else {
-//         req.flash("error", "Something went wrong!!");
-//         res.redirect("/signin");
-//     }
-//     // res.locals.message = req.flash();
-
-//     // res.redirect("/signup");
-// })
-
-
-// route middleware to make sure
-function isLoggedIn(req, res, next) {
-    // if user is authenticated in the session, carry on
-    if (req.isAuthenticated())
-        return next();
-    // if they aren't redirect them to the home page
-    res.redirect('/');
-}
 
 module.exports = router;
